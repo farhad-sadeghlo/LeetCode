41e209f4a6cb407f191e6bf003dfada15edf9445
TheLion <farhad.sadeghlo@gmail.com>
2022-08-03 14:01:54 -0400

    Code added for Word_Subsets

---
+++ Q916Word_Subsets.py
@@ -0,0 +1,13 @@
+class wordSubsets:
+
+    def __init__(self, words, chars):
+
+        self.words = words.replace("]", "").replace("[", "").replace('"', '').split(',')
+        self.chars = chars.replace("]", "").replace("[", "").replace('"', '').split(',')
+        self.lst_words = []
+
+    def subsets(self):
+
+        self.lst_words = [word for word in self.words if all(char in word for char in self.chars)]
+
+        return print(self.lst_words)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import roman_to_integer
-from valid_anagram import validAnagram
+from Q13roman_to_integer import roman_to_integer
+from Q242valid_anagram import validAnagram
+from Q916Word_Subsets import wordSubsets
 
 if __name__ == '__main__':
-    # roman_to_integer.roman_to_integer(input('please type in your roman number: ')).romanToInt()
-    validAnagram().anagram()
+    # roman_to_integer(input('please type in your roman number: ')).romanToInt()
+    # validAnagram().anagram()
+    wordSubsets(input('please type in list of your words: '), input('please type in list of your chars: ')).subsets()
