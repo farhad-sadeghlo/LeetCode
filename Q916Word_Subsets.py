0995cf24ea163877193653ecf3fba698e37223ae
TheLion <farhad.sadeghlo@gmail.com>
2022-08-03 23:07:46 -0400

    Code added for Word Subsets

--- Q916Word_Subsets.py
+++ Q916Word_Subsets.py
@@ -1,14 +1,23 @@
+from collections import Counter
 class wordSubsets:
 
-    def __init__(self, words, chars):
+    def __init__(self, words, strings):
 
         self.words = words.replace("]", "").replace("[", "").replace('"', '').split(',')
-        self.chars = chars.replace("]", "").replace("[", "").replace('"', '').split(',')
+        self.strings = strings.replace("]", "").replace("[", "").replace('"', '').split(',')
         self.lst_words = []
 
     def subsets(self):
 
-        self.lst_words = [word for word in self.words if all(char in word for char in self.chars)]
+        dic = {}
+        for a_string in self.strings:
+            for char, num in Counter(a_string).items():
+                if char not in dic.keys() or dic[char] < num:
+                    dic[char] = num
+        print(dic)
+        for word in self.words:
+            word_counts = Counter(word)
+            if all(char in word_counts and num <= word_counts[char] for char, num in dic.items()):
+                self.lst_words.append(word)
 
         return print(self.lst_words)
- 
